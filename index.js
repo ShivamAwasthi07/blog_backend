679e7d5c7cae5b2d3523f07f6edf65eee027f38f
Shivam Awasthi <shivam@olibr.com>
2023-04-03 01:30:28 +0530

    backend

--- index.js
+++ index.js
@@ -34,14 +34,64 @@ app.get("/blogs", async (req, res) => {
   });
 });
 
+app.get("/blogs/:blogId", async (req, res) => {
+  const q = "SELECT * FROM blogs where blogId = (?)";
+  const id = req.params.blogId;
+  await db.query(q, [id], (err, data) => {
+    if (err) {
+      return res.json({
+        statusCode: 500,
+        statusMessage: "FAILED",
+        message: "Data could not be fetched",
+        data: err,
+      });
+    }
+    return res.json({
+      statusCode: 200,
+      statusMessage: "SUCCESS",
+      data: data ? data[0] : {},
+      message: "Blog Fetched Successfully",
+    });
+  });
+});
+
+app.delete("/blogs/:blogId", async (req, res) => {
+  const q = "DELETE FROM blogs where blogId = (?)";
+  const id = req.params.blogId;
+  await db.query(q, [id], (err, data) => {
+    if (err) {
+      return res.json({
+        statusCode: 500,
+        statusMessage: "FAILED",
+        message: "Data could not be deleted",
+        data: err,
+      });
+    }
+    if (data.affectedRows === 0) {
+      return res.json({
+        statusCode: 200,
+        statusMessage: "FAIL",
+        message: "Blog cannot be found in the list",
+      });
+    }
+    return res.json({
+      statusCode: 200,
+      statusMessage: "SUCCESS",
+      message: "Blog Deleted Successfully",
+    });
+  });
+});
+
 app.post("/blogs", async (req, res) => {
   const query =
-    "INSERT INTO blogs (`blogTitle`, `blogText`, `authorName`, `postDate`) VALUES (?)";
+    "INSERT INTO blogs (`blogTitle`, `blogText`, `authorName`, `postDate`, `blogDesc`, `coverImage`) VALUES (?)";
   const values = [
     req.body.blogTitle,
     req.body.blogText,
     req.body.authorName,
     req.body.postDate,
+    req.body.blogDesc,
+    req.body.coverImage,
   ];
   await db.query(query, [values], (err, data) => {
     if (err) {
